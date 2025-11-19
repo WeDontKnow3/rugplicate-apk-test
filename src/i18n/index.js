0facf306e508ea1cbd1a2d380a04dace125fc4c0
WeDontKnow3 <marquinhorelampago004@gmail.com>
2025-11-19 11:43:22 -0300

    Create index.js

---
+++ src/i18n/index.js
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './en.json';
+import pt from './pt.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      pt: { translation: pt }
+    },
+    fallbackLng: 'pt',
+    detection: {
+      order: ['localStorage', 'navigator', 'querystring', 'cookie', 'htmlTag'],
+      caches: ['localStorage']
+    },
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
